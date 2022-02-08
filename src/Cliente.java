d3ce199cdfe88554d48c57b43ff7bfc14d9b7470
Alberto <alberto01bp@gmail.com>
2022-02-08 17:11:11 +0000

    Feature: el cliente interactua con el servidor correctamente, tambien se arregla´ón unos fallos

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,10 +2,12 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.*;
 
 public class Cliente {
     public static void main(String[] args) throws IOException {
+        List<String> mensajes = Collections.synchronizedList(new ArrayList());
+
         Scanner leer = new Scanner(System.in);
         ObjectInputStream ois = null;
         ObjectOutputStream oos = null;
@@ -14,18 +16,47 @@ public class Cliente {
         ois = new ObjectInputStream(socket.getInputStream());
 
         try {
+            /* Enviar usuario */
             System.out.println("Introduce tu nombre: ");
-            oos.writeObject(leer.nextLine());
-
+            String nombreUsuario = leer.nextLine();
+            oos.writeObject(nombreUsuario);
+            /* Obtener el saludo */
             String returnValue = (String) ois.readObject();
             System.out.println(returnValue);
 
+            /* Solicito la lista de mensajes*/
+            String textorecogido = (String) ois.readObject();
+            System.out.println("Mensajes recogidos ....");
+            StringTokenizer nuevosMensajes = new StringTokenizer(textorecogido, "***");
+
+            while (nuevosMensajes.hasMoreTokens()){
+                System.out.println(nuevosMensajes.nextToken());
+            }
+
+
             String texto = "";
             while ( !texto.equals("Good bye") ){
+
                 System.out.println("Introduce una interacción: ");
-                oos.writeObject(leer.nextLine());
-                texto = (String) ois.readObject();
-                System.out.println("El servidor responde => " + texto);
+                texto = leer.nextLine();
+
+                if (texto.equals("bye")) {
+                    oos.writeObject(texto);
+                    texto = (String) ois.readObject();
+                }else if (texto.contains("message:")) {
+
+                    oos.writeObject(texto);
+
+                    String Textonuevo = (String) ois.readObject();
+                    StringTokenizer primerosMensajes = new StringTokenizer(Textonuevo, "***");
+
+                    while (primerosMensajes.hasMoreTokens()){
+                        System.out.println(primerosMensajes.nextToken());
+                    }
+
+                }  else {
+                    System.out.println("Error en los datos intorducidos");
+                }
             }
 
         } catch (ClassNotFoundException e) {
